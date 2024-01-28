3df89c72fd33280f7f014f29e598c57a62d637fd
Don MacSween <donmacsween@gmail.com>
2024-01-28 23:40:43 +0000

    Final  28/1

---
+++ Assets/_Game/Scripts/UI/MainMenu/MapRoller.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MapRoller : MonoBehaviour
+{
+    Renderer rend;
+    [SerializeField] private float      closedRollValue = 0.065f;
+    [SerializeField] private float      openRollValue   = 1.0f;
+    [SerializeField] private float      rollSpeed       = 0.1f;
+    [SerializeField] private float      rollValue;
+    [SerializeField] private GameObject mapButtons;
+                     private bool        unrolling       = true;
+    void OnEnable()
+    {
+        rend = GetComponent<Renderer>();
+        rollValue = closedRollValue;
+        rend.material.shader = Shader.Find("Shader Graphs/MapRollDoubleSidedURP");
+        mapButtons.SetActive(false);
+        rend.material.SetFloat("Vector1_98d33b1d219b486e97f4a6d459a007a3", closedRollValue);
+    }
+    void Update()
+    {       
+           if (rollValue >= closedRollValue && unrolling==false)
+           {
+                rollValue -= rollSpeed;
+                rend.material.SetFloat("Vector1_98d33b1d219b486e97f4a6d459a007a3", rollValue);
+           }
+            
+        if ((rollValue < openRollValue) && unrolling == true)
+        {
+                rollValue += rollSpeed;
+                rend.material.SetFloat("Vector1_98d33b1d219b486e97f4a6d459a007a3", rollValue);
+        }
+        if (rollValue >= 1) {mapButtons.SetActive(true);}
+        if (!unrolling) {mapButtons.SetActive(false);}
+    }
+    public void Rollup()
+    {
+        unrolling = false;
+    }
+    public void UnRoll()
+    {
+        unrolling = true;
+    }
+}

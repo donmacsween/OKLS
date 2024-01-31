343b015786373cdbcd3cff284f139b02c90cfa01
Don Macsween <DM1200@student.aru.ac.uk>
2024-01-31 14:13:42 +0000

    input

---
+++ Assets/_Game/Scripts/Managers/InputManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    private Ray         ray;
+    private RaycastHit  hit;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       if (camera == null)
+        {
+            camera = Camera.main;
+        }
+
+        
+    }
+
+    public void Click ()
+    {        
+        ray = camera.ScreenPointToRay(Pointer.current.position.value);
+        if(Physics.Raycast(ray, hitInfo: out hit) && hit.collider)
+        {    
+            // Tower
+
+            // Path
+
+            // anything else
+
+
+
+        }
+    }
+
+    // drag
+
+    public void Power1()
+    {
+
+    }
+    public void Power2()
+    {
+
+    }
+    public void Power3()
+    {
+
+    }
+    public void Menu()
+    {
+
+    }
+
+
+}
